b48abbef2ce4b69c4d8673439c58bece773eab62
lucasmelloec <lucasmelloec@gmail.com>
2025-07-25 17:37:28 -0300

    fiddling with colors and resize

--- src/handmade.cpp
+++ src/handmade.cpp
@@ -1,14 +1,27 @@
 #include "raylib.h"
 
+static bool running;
+
 int main() {
+  SetConfigFlags(FLAG_WINDOW_RESIZABLE);
   InitWindow(800, 600, "Handmade Hero");
   SetExitKey(KEY_NULL);
   while (!WindowShouldClose()) {
-    BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawText("Congrats! You created your first window!", 190, 200, 20,
-                LIGHTGRAY);
-    EndDrawing();
+    if (WindowShouldClose()) {
+      running = false;
+    } else {
+      static Color my_color = WHITE;
+      BeginDrawing();
+      ClearBackground(my_color);
+      DrawText("Congrats! You created your first window!", 190, 200, 20,
+               LIGHTGRAY);
+      EndDrawing();
+      if (my_color.r == WHITE.r) {
+        my_color = BLACK;
+      } else {
+        my_color = WHITE;
+      }
+    }
   }
 
   return 0;
